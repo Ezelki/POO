79ac1cff9a205b129965d4802dd866ff3fdc7320
bionarede <bionarede32@gmail.com>
2021-11-30 15:26:03 -0600

    VRUM VRUM

--- src/EzequielHenrique/Main.java
+++ src/EzequielHenrique/Main.java
@@ -1,8 +1,5 @@
 package EzequielHenrique;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  *
  * @author Ezequiel Henrique
@@ -10,23 +7,22 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-
-        Carro carro = new Carro();
-        carro.setCor("Azul");
-        carro.setNumPortas(2);
-        carro.setPlaca("AAA-0001");
-        carro.setTipo("Chevette");
-        
-        carro.impC();
-
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("Jorge");
         pessoa.setSobrenome("Augusto");
         pessoa.setIdade(32);
         pessoa.setPeso(69.23);
-        carro.setPessoa(dono);
+
         pessoa.impP();
         
+        Carro carro = new Carro();
+        carro.setCor("Azul");
+        carro.setNumPortas(2);
+        carro.setPlaca("AAA-0001");
+        carro.setTipo("Chevette");
+        carro.setDono(pessoa);
+        carro.impC();
+
         pessoa.setCarro(carro);
         pessoa.ligarCarro();
         pessoa.setCambioMarcha(1);
@@ -36,9 +32,7 @@ public class Main {
         pessoa.setCambioMarcha(3);
         pessoa.frearCarro();
         pessoa.getCarro().getCambio();
-        
-        
-        
+
     }
 
 }
